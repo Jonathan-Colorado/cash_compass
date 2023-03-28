3addd68f83c5f701d1be05b76849c943d0bd262c
Jonathan-Colorado <63272588+Jonathan-Colorado@users.noreply.github.com>
2023-03-28 17:19:52 -0500

    minor formatting

--- backend/main.py
+++ backend/main.py
@@ -9,10 +9,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 from routers.accounts import router as accounts_router
 
-
 DB_URL = config("DB_URL", cast=str)
 DB_NAME = config("DB_NAME", cast=str)
-
 
 # define origins
 origins = ["*"]
@@ -29,19 +27,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
 @app.on_event("startup")
 async def startup_db_client():
     app.mongodb_client = AsyncIOMotorClient(DB_URL)
     app.mongodb = app.mongodb_client[DB_NAME]
 
-
 @app.on_event("shutdown")
 async def shutdown_db_client():
     app.mongodb_client.close()
 
-
 app.include_router(accounts_router, prefix="/accounts", tags=["accounts"])
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True)
--- backend/routers/accounts.py
+++ backend/routers/accounts.py
@@ -2,18 +2,15 @@ from typing import List
 from fastapi import APIRouter, Request, Body, status, HTTPException
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
-
-
 from models import AccountBase, AccountDB, AccountUpdate
-
 
 router = APIRouter()
 
 @router.get("/", response_description="List all accounts")
 async def list_all_accounts(
-    request: Request, 
-    min_price: int=0, 
-    max_price:int=100000, 
+    request: Request,
+    min_price: int=0,
+    max_price:int=100000,
     rate: float | None = None,
     page:int=1,
     ) -> List[AccountDB]:
@@ -24,18 +21,14 @@ async def list_all_accounts(
     query = {"price":{"$lt":max_price, "$gt":min_price}}
     if rate:
         query["rate"] = rate
-    
     full_query = request.app.mongodb['accounts'].find(query).sort("_id",-1).skip(skip).limit(RESULTS_PER_PAGE)
-
     results = [AccountDB(**raw_account) async for raw_account in full_query]
-    
     return results
 
 # create new account
 @router.post("/", response_description="Add new account")
 async def create_account(request: Request, account: AccountBase = Body(...)):
     account = jsonable_encoder(account)
-  
     new_account = await request.app.mongodb["accounts"].insert_one(account)
     created_account = await request.app.mongodb["accounts"].find_one(
         {"_id": new_account.inserted_id}
@@ -51,21 +44,16 @@ async def show_account(id: str, request: Request):
 
 @router.patch("/{id}", response_description="Update account")
 async def update_task(id: str, request: Request, account: AccountUpdate = Body(...)):
-  
     await request.app.mongodb['accounts'].update_one(
         {"_id": id}, {"$set": account.dict(exclude_unset=True)}
     )
     if (account := await request.app.mongodb['accounts'].find_one({"_id": id})) is not None:
         return AccountDB(**account)
-
     raise HTTPException(status_code=404, detail=f"Account with {id} not found")
-
 
 @router.delete("/{id}", response_description="Delete account")
 async def delete_task(id: str, request: Request):
     delete_result = await request.app.mongodb['accounts'].delete_one({"_id": id})
-
     if delete_result.deleted_count == 1:
         return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
-
     raise HTTPException(status_code=404, detail=f"Account with {id} not found")
